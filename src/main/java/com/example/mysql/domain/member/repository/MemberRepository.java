320b69d2444be0c4c168aff950682003d2c3d79d
fxzz <fxzzy3@gmail.com>
2023-07-04 12:36:02 +0900

    #1 - 회원정보 등록 구현

---
+++ src/main/java/com/example/mysql/domain/member/repository/MemberRepository.java
@@ -0,0 +1,50 @@
+package com.example.mysql.domain.member.repository;
+
+import com.example.mysql.domain.member.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    static final String TABLE = "user";
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public Member save(Member member) {
+        if (member.getId() == null)
+            return insert(member);
+        return update(member);
+    }
+
+    private Member insert(Member member) {
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName(TABLE)
+                .usingGeneratedKeyColumns("id");
+
+        SqlParameterSource params = new BeanPropertySqlParameterSource(member);
+        var id = jdbcInsert.executeAndReturnKey(params).longValue();
+
+        return Member.builder()
+                .id(id)
+                .nickname(member.getNickname())
+                .email(member.getEmail())
+                .birthday(member.getBirthday())
+                .build();
+    }
+
+    private Member update(Member member) {
+        var sql = String.format("UPDATE `%s` set email = ?, nickname = ?, birthday = ? WHERE id = ?", TABLE);
+        jdbcTemplate.update(sql, member.getEmail(), member.getNickname(), member.getBirthday(), member.getId());
+        return member;
+    }
+}
+
---
+++ src/main/java/com/example/mysql/shcema/ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE member (
+                        id BIGINT AUTO_INCREMENT,
+                        email VARCHAR(20) NOT NULL,
+                        nickname VARCHAR(20) NOT NULL,
+                        birthday DATE NOT NULL,
+                        createdAt DATETIME NOT NULL,
+                        PRIMARY KEY (id)
+);
